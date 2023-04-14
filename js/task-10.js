040c683b75f43978acb75c0993135e2ec7b294cb
Illia Shatokhin <resonance.2106@gmail.com>
2023-04-14 19:07:21 +0300

    first finish

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const boxContainer = document.getElementById("boxes");
+const controlsContainer = document.getElementById("controls");
+let inputValue = 0;
+let width = 30;
+let height = 30;
+
+
+
+controlsContainer.firstElementChild.addEventListener("input", () => {
+  inputValue = Number(controlsContainer.firstElementChild.value);
+});
+controlsContainer.children[1].addEventListener("click", () => createBoxes(inputValue));
+controlsContainer.lastElementChild.addEventListener("click", () => destroyBoxes());
+
+
+
+function createBoxes(amount) {
+  let str = "";
+  for (let i = 0; i < amount; i++) {
+    const hexColor = getRandomHexColor();
+    str += `<div style="background-color: ${hexColor}; width: ${width}px; height: ${height}px;">a</div>`;
+    width += 10;
+    height += 10;
+  }
+  boxContainer.insertAdjacentHTML("beforeend", str);
+};
+
+function destroyBoxes() {
+  boxContainer.innerHTML = "";
+  width = 30;
+  height = 30;
+};
